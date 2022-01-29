1a2f797a9029d62f29df7c8c7d3e7d8e8e0a53d5
lnguyen87 <luc_nguyen@outlook.com>
2022-01-29 10:41:31 -0700

    connected db to server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const inquirer = require('inquirer');
-// const mysql = require('mysql');
-const express = require('express');
+// const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,11 +9,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL usernae,
+    user: "root",
+    // Your MySQL password
+    password: "password",
+    database: "employee_tracker",
+  },
+  console.log("Connected to the employee tracker database.")
+);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
